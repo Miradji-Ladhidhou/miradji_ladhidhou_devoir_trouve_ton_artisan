9ac1c647036fe321cefae87e0a6a999969ec9233
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-05-04 17:03:09 +0400

    parametrage .env

--- api/config/database.js
+++ api/config/database.js
@@ -1,19 +1,24 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize("artisans", "root", "root", {
-  host: "localhost",   
-  port: 8889,         
-  dialect: "mysql",
-  logging: false,      
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 sequelize.authenticate()
   .then(() => {
-    console.log('Connexion à la base de données réussie!');
+    console.log("Connexion à la base de données réussie !");
   })
   .catch((err) => {
-    console.error('Impossible de se connecter à la base de données:', err);
+    console.error("Impossible de se connecter à la base de données :", err);
   });
-
 
 module.exports = sequelize;
